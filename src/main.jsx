7f14583a35622667fc8767f913a686d0a79c8d38
ZionElira <suraj.ziontech@gmail.com>
2024-12-24 11:31:49 +0530

    change in file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client'; // Correct import
+import './index.css';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-
-  <BrowserRouter >
+createRoot(document.getElementById('root')).render( // Use createRoot directly
+  <BrowserRouter>
     <App />
   </BrowserRouter>
-
-)
+);
